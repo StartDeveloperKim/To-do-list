3fb90ab4f16626599d149445ced39d8bce27f53f
StartDeveloperKim <ktw550111@naver.com>
2022-12-22 16:49:49 +0900

    Feat. 로그인, 회원가입 페이지 및 API 작성

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import Todo from "./Todo";
 import React, { useEffect, useState } from "react";
-import { Container, List, Paper } from "@material-ui/core";
+import { Container, List, Paper, Grid, Button, AppBar, Toolbar, Typography, CircularProgress, } from "@material-ui/core";
 import AddTodo from "./AddTodo";
-import { call } from "./ApiService";
+import { call, signout } from "./ApiService";
 
 function App() {
   // Todo 배열
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    call("/todo", "GET", null)
-    .then((response) => setItems(response.data));
+    call("/todo", "GET", null).then((response) => {
+      setItems(response.data); 
+      setLoading(false)
+    });
   }, []);
 
   const addItem = (item) => {
@@ -28,8 +31,27 @@ function App() {
     call("/todo", "PUT", item)
     .then((response) => setItems(response.data));
   };
+
+  // 네비게이션 Bar
+  let navigationBar = (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid jstifyContent="space-between" container>
+          <Grid item>
+            <Typography variant="h6">오늘의 할일</Typography>
+          </Grid>
+          <Grid item>
+            <Button color="inherit" raised onClick={signout}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
   
 
+  // Todo 아이템
   let todoItems = items.length > 0 && (
     <Paper style={{ margin: 16 }}>
       <List>
@@ -39,14 +61,33 @@ function App() {
       </List>
     </Paper>
   );
+
+  let todoListPage = (
+    <div>
+      {navigationBar}
+      <Container maxWidth="md">
+        <AddTodo addItem={addItem} />
+        <div className="TodoList">{todoItems}</div>
+      </Container>
+    </div>
+  );
+
+  let loadingPage = (
+    <div>
+      <CircularProgress />
+      <h2>로딩 중..</h2>
+    </div>
+      
+      );
+  let content = loadingPage;
+
+  if (!loading) {
+    content = todoListPage;
+  }
+
   return (
     <div className='App'>
-      <Container maxWidth="md">
-        <AddTodo addItem={addItem}/>
-        <div className="TodoList">
-          {todoItems}
-        </div>
-      </Container>
+      {content}
     </div>
   );
 }
